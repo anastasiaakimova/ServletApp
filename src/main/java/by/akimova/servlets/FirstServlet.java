cf5834befcb16f022779045073281387be489280
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-04-19 13:09:35 +0400

    add user and update jsp

--- src/main/java/by/akimova/servlets/FirstServlet.java
+++ src/main/java/by/akimova/servlets/FirstServlet.java
@@ -1,5 +1,7 @@
 package by.akimova.servlets;
 
+import by.akimova.model.User;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,21 +9,59 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @WebServlet("/hello")
 public class FirstServlet extends HttpServlet {
+
+    private static final String index = "/WEB-INF/view/index.jsp";
+    private List<User> users;
+
     @Override
     public void init() throws ServletException {
+        users = new CopyOnWriteArrayList<>();
+        users.add(new User("Java", 10));
+        users.add(new User("Vision", 20));
         System.out.println("Servlet is init");
-
     }
 
+    // отображаем страницу
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("index.jsp");
-        requestDispatcher.forward(req, resp);
+        req.setAttribute("users", users);
+        req.getRequestDispatcher(index).forward(req, resp);
         System.out.println("doGet is working");
+    }
+
+    // получение данных с формы
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setCharacterEncoding("UTF8");
+        if (!requestIsValid(req)) {
+            doGet(req, resp);
+
+        }
+
+        final String name = req.getParameter("name");
+        final String age = req.getParameter("age");
+        final User user = new User(name, Integer.valueOf(age));
+
+        users.add(user);
+        doGet(req, resp);
+
+        System.out.println("doPost is working");
+    }
+
+    private boolean requestIsValid(final HttpServletRequest req) {
+        final String name = req.getParameter("name");
+        final String age = req.getParameter("age");
+
+        return name != null && name.length() > 0 &&
+                age != null && age.length() > 0 &&
+                age.matches("[+]?\\d+");
     }
 
     @Override
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,25 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html; charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>Hello page</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h1>Hello from Anastasia Akimova!</h1> <br/>
+<h2>All users</h2><br/>
+<c:forEach var="user" items="${requestScope.users}">
+    <ul>
+        <li>Name: <c:out value="${user.name}"/></li>
+        <li>Age: <c:out value="${user.age}"/></li>
+    </ul>
+    <hr/>
+</c:forEach>
+
+<h2>Create user</h2><br/>
+<form method="post" action="">
+    <label><input type="text" name="name"></label>Name<br>
+    <label><input type="number" name="age"></label>Age<br>
+    <input type="submit" value="ok" name="ok"><br>
+</form>
 </body>
 </html>
